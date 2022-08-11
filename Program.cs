afd0b3fa9818b68c72f4013f728241d19606135e
Hema <hemasahu019@gmail.com>
2022-08-11 09:32:36 +0530

    Added UC2 Code to determine user input

--- Program.cs
+++ Program.cs
@@ -1,10 +1,15 @@
 ﻿namespace TicTacToeBoardGame
 {
     internal class Program
-    {
+    {   
+        //An entrypoint of consle app
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Welcome to TacTacToe Board Game");
+
+            TicTacToeGame ticTacToeObj = new TicTacToeGame(); //created object
+            ticTacToeObj.Uc1(); //invoking Uc1method
+            ticTacToeObj.Uc2(); //invoking Uc2method
         }
     }
 }
