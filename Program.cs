62bdd03ed21bf3667d0a58a305737088aebb41e4
Hema <hemasahu019@gmail.com>
2022-08-11 08:42:04 +0530

    Added UC1 Code for Tic Tac Toe Board Game

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace TicTacToeBoardGame
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ TicTacToeGame.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicTacToeBoardGame
+{
+    public class TicTacToeGame
+    {   
+        //method for UC1
+        public void Uc1()
+        {
+            char[] board = new char[10];
+            
+            //loop to initalize space
+            for(int i = 1; i<= 10; i++)
+            {
+                board[i] = ' ';
+            }
+        }
+    }
+}
